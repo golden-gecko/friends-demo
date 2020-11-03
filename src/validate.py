c53cfb9c209bd19e762095893fa10014ecd4087d
Wojciech Holisz <info@holisz.pl>
2020-11-03 16:33:56 +0100

    Add tests

--- src/validate.py
+++ src/validate.py
@@ -1,2 +1,7 @@
 def validate_user_id(user_id: int) -> bool:
-    return 0 < user_id < 2147483647
+    try:
+        int(user_id)
+    except ValueError:
+        return False
+    else:
+        return 0 < user_id < 2147483647
