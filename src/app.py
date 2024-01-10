2258ced8332db8c786ed47883971f39114f6ec9d
Wojciech Holisz <golden.gecko@holisz.pl>
2024-01-10 23:11:41 +0100

    Upgrade

--- src/app.py
+++ src/app.py
@@ -1,25 +1,15 @@
-import connexion
-import os
+from connexion import FlaskApp
 
-from models import db
+app = FlaskApp(__name__)
+app.add_api('v1.yaml')
 
-
-def create_app(specification_dir: str) -> tuple:
-    connexion_app = connexion.App(__name__, specification_dir=specification_dir)
-
-    app = connexion_app.app
-    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
-    app.config['SQLALCHEMY_ECHO'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
-        os.environ.get('POSTGRES_USER'), os.environ.get('POSTGRES_PASSWORD'),
-        os.environ.get('POSTGRES_HOST'), os.environ.get('POSTGRES_DATABASE_NAME')
-    )
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-    return connexion_app, app
-
-
-connexion_app, app = create_app(specification_dir=os.path.abspath(os.path.dirname(__file__)))
-connexion_app.add_api('v1.yaml', validate_responses=True)
+flask_app = connexion_app.app
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
+    os.environ.get('POSTGRES_USER'), os.environ.get('POSTGRES_PASSWORD'),
+    os.environ.get('POSTGRES_HOST'), os.environ.get('POSTGRES_DATABASE_NAME')
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db.init_app(app)
